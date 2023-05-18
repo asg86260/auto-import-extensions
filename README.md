1f71a288d5c39668140f55b28ab98fce4ad7e90c
Andrew Graham <agraham@pollyex.com>
2023-05-18 18:21:35 -0500

    init proj

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # auto-import-extensions
 Script to help with the vue@2 -> vue@3 upgrade. analyze imports without file extensions and auto detect their extension.
+
+This is a very barebones script that I wrote to help with upgrading our project from vue 2 to vue 3. Since vue 3 requires all imports to have file extensions (.vue|.js, etc)
+and I didn't want to manually update all our imports this script auto detected the imported modules filetype and updates the import.
+
+We only have .vue and .js files so thats all i needed to handle. this could be updated to find the file, read the ext from `fs` and use that instead to support more .ts, .cjs, whatever else.
+
+This also reads your aliases from your `vite.config.js` which it's assuming is in the root of your project. 
---
+++ index.js
@@ -0,0 +1,101 @@
+import fs from 'fs';
+import path from 'path';
+import config from '../vite.config.js';
+const { resolve } = config;
+
+const projectDirectory = './src'; // Replace with your project's directory
+
+// Function to recursively find JavaScript and Vue files
+function findFiles(dir, filePattern) {
+  let files = [];
+
+  fs.readdirSync(dir).forEach((file) => {
+    const filePath = path.join(dir, file);
+    const fileStat = fs.statSync(filePath);
+
+    if (fileStat.isDirectory()) {
+      files = files.concat(findFiles(filePath, filePattern));
+    } else if (filePattern.test(file)) {
+      files.push(filePath);
+    }
+  });
+
+  return files;
+}
+
+// Function to add file extension to module imports
+function addFileExtensions(files) {
+  files.forEach((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Error reading file: ${file}`);
+        console.error(err);
+        return;
+      }
+      const updatedData = data.replaceAll(
+        // eslint-disable-next-line no-useless-escape
+        /(import\s+{?[^}]*?}?\s+from\s+['"])([^.'"]+)(['"]\s*;)|(import\s*\([\s\S]*?['"])([^'"]+[^/])(['"]\s*[\s\S]*?\)\s*)/g,
+        (match, p1, p2, p3, p4, p5, p6) => {
+          console.log({ p1, p2, p3, p4, p5, p6 });
+          const g1 = p1 || p4;
+          const g2 = p2 || p5;
+          const g3 = p3 || p6;
+          let filePath = path.join(path.dirname(file), g2);
+          if (!g2.startsWith('.')) {
+            filePath = resolveAlias(g2);
+          }
+          const jsFile = fs.existsSync(`${filePath}.js`);
+
+          const vueFile = fs.existsSync(`${filePath}.vue`);
+
+          const ifIndexExists = fs.existsSync(`${filePath}/index.js`);
+
+          if (jsFile) {
+            return `${g1}${g2}.js${g3}`;
+          }
+
+          if (vueFile) {
+            return `${g1}${g2}.vue${g3}`;
+          }
+
+          if (ifIndexExists) {
+            return `${g1}${g2}${g3}`;
+          }
+          return match;
+        },
+      );
+
+      if (data !== updatedData) {
+        fs.writeFile(file, updatedData, 'utf8', (writeErr) => {
+          if (writeErr) {
+            console.error(`Error writing file: ${file}`);
+            console.error(writeErr);
+          } else {
+            console.log(`File updated: ${file}`);
+          }
+        });
+      }
+    });
+  });
+}
+
+// Function to resolve aliases from vite.config.js
+function resolveAlias(filePath) {
+  const viteConfigPath = path.join('vite.config.js');
+  if (fs.existsSync(viteConfigPath)) {
+    if (resolve && resolve.alias) {
+      for (const alias in resolve.alias) {
+        if (filePath.startsWith(alias)) {
+          const resolvedPath = filePath.replace(alias, resolve.alias[alias]);
+          return resolvedPath;
+        }
+      }
+    }
+  }
+  return filePath;
+}
+
+// Entry point
+const filePattern = /\.(js|vue)$/i;
+const files = findFiles(projectDirectory, filePattern);
+addFileExtensions(files);
